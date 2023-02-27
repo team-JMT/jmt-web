b22f5606249c82b7a0a71a0df878f8346904240a
bjc1102 <bjc30849@gmail.com>
2023-02-27 11:00:40 +0900

    chore: styles path alias

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import App from './App';
-import './index.scss';
+import '@styles/index.scss';
+import '@styles/_normalize.scss';
 
 const queryClient = new QueryClient();
 
