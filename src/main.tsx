f06de4514c53b97357114e37c55cc7fa1871eada
HyeokE <jhjeong00@gmail.com>
2023-02-28 20:51:04 +0900

    feat: add vconsole

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import VConsole from 'vconsole';
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
@@ -9,6 +11,8 @@ import '@styles/index.scss';
 import '@styles/_normalize.scss';
 
 const queryClient = new QueryClient();
+
+const vConsole = new VConsole();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
