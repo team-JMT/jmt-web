55499d13fe0f0c5c0f222ee619c8e23eed30c7e5
Byeong Ju Choi <71929440+bjc1102@users.noreply.github.com>
2023-03-20 19:42:34 +0900

    Feature/jmt 19 aos bridge (#10)
    Co-authored-by: HyeokE <jhjeong00@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { useState } from 'react';
+import React from 'react';
 
 import reactLogo from './assets/react.svg';
 import '@styles/App.scss';
+import { getAccessToken } from './utils/bridge';
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const getToken = () => {
+    getAccessToken();
+  };
   return (
     <div className="App">
       <div>
@@ -18,9 +20,6 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -28,6 +27,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <button onClick={getToken}>Get Token123</button>
     </div>
   );
 }
---
+++ src/utils/bridge.ts
@@ -0,0 +1,51 @@
+import { nativeInfo } from './storage';
+
+function setAccessToken(accessToken: string) {
+  console.log('setAccessToken', accessToken);
+  nativeInfo.setData({
+    accessToken: accessToken,
+  });
+}
+
+function backEvent() {
+  console.log('backEvent');
+  history.back();
+}
+
+if (window) {
+  window.setAccessToken = setAccessToken;
+  window.backEvent = backEvent;
+}
+
+export function getAccessToken() {
+  if (window.webkit) {
+    window.webkit.messageHandlers.callbackHandler.postMessage(
+      JSON.stringify({
+        event: 'token',
+      }),
+    );
+  } else {
+    window?.webviewBridge?.token();
+  }
+}
+
+export function backSwipe() {
+  if (window.webkit) {
+    window.webkit.messageHandlers.callbackHandler.postMessage(
+      JSON.stringify({
+        event: 'back',
+      }),
+    );
+  }
+}
+
+export function setToEnabledSwipe(enabled = true) {
+  if (window.webkit) {
+    window.webkit.messageHandlers.callbackHandler.postMessage(
+      JSON.stringify({
+        event: 'swipe',
+        isEnableSwipe: enabled,
+      }),
+    );
+  }
+}
