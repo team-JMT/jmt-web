31ea1f11bf5e289a4bd5566105ad751e61f75345
HyeokE <jhjeong00@gmail.com>
2023-03-28 21:22:46 +0900

    fix: input motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 
 import Router from './Routes/Router';
-import { getAccessToken } from './utils/bridge';
 
 function App() {
-  getAccessToken();
+  // getAccessToken();
 
   return <Router />;
 }
