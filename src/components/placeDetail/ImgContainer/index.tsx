69318ac4565982ce2e2260a77812089d4f4b3574
jinhye <jinh2376@gmail.com>
2023-05-08 11:59:35 +0900

    feat: bootstrap Carousel

--- src/components/placeDetail/ImgContainer/index.tsx
+++ src/components/placeDetail/ImgContainer/index.tsx
@@ -1,46 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Carousel } from 'react-bootstrap';
 
 import styled from '@emotion/styled';
-import testImg from './testImg.png';
+import 'bootstrap/dist/css/bootstrap.css';
 
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import testImg from './img1.png';
+const imgArray = ['./img1.png', './img2.png', './img3.png'];
 
-const imgArray = ['./testImg.png', './testImg.png', './testImg.png'];
 const ImgContainer = () => {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex: number) => {
+    setIndex(selectedIndex);
+  };
+
   return (
-    <div
-      id="carouselExampleSlidesOnly"
-      className="carousel slide"
-      data-ride="carousel"
-    >
-      <div className="carousel-inner">
-        {imgArray.map(
-          (
-            item,
-            index, //active 설정 해ㅈ줘야 함
-          ) => (
-            <div
-              className={`carousel-item ${index === 0 ? 'active' : ''}`}
-              key={index}
-            >
-              <img className={'d-block w-100'} src={testImg} alt={item} />
-            </div>
-          ),
-        )}
-      </div>
-    </div>
+    <ImgWrapper>
+      <StyledCarousel
+        activeIndex={index}
+        onSelect={handleSelect}
+        interval={null} //자동으로 돌아가지 않도록
+        prevIcon={<span className="visually-hidden"></span>}
+        nextIcon={<span className="visually-hidden"></span>}
+        indicators={false}
+      >
+        {imgArray.map((item, index) => (
+          <Carousel.Item key={index}>
+            <PlaceImg imgUrl={testImg} />
+          </Carousel.Item>
+        ))}
+      </StyledCarousel>
+      <ImgNumber className={'text-m-medium'}>
+        {index + 1} / {imgArray.length}
+      </ImgNumber>
+    </ImgWrapper>
   );
 };
 
-{
-  /*        <DotContainer>
-{imageArray.map((index) => {
-<div style={{opacity: 1 - Math.abs((activeIndex - index) * 0.2) }} />
-</DotContainer>
-); */
-}
-const Image = styled.div<{ imgUrl: string }>`
-  width: 100%;
+const StyledCarousel = styled(Carousel)`
+  //indicator 스타일링
 `;
+const ImgWrapper = styled.div`
+  position: relative;
+`;
+const PlaceImg = styled.div<{ imgUrl: string }>`
+  width: 100%;
+  height: calc(100vw * 0.67);
+  background: url(${(props) => props.imgUrl});
+  background-size: cover;
+`;
+const ImgNumber = styled.div`
+  position: absolute;
+  right: 11px;
+  bottom: 7px;
+  color: white;
+`;
+
 export default ImgContainer;
