e59de28bfa703fb9dfe4544f7cb2cf5b99de0fa0
HyeokE <jhjeong00@gmail.com>
2023-05-21 21:29:43 +0900

    feat: add modal

---
+++ src/components/common/Modal/components/Wrapper/styled.tsx
@@ -0,0 +1,55 @@
+import styled from '@emotion/styled';
+import { motion } from 'framer-motion';
+
+export const LayerWrapper = styled(motion.div)`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: rgba(22, 26, 29, 0.3);
+  opacity: 1;
+  content: '';
+  z-index: 100;
+`;
+
+export const Inner = styled(motion.div)`
+  overflow: hidden;
+  position: fixed;
+  min-height: 100px;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 1.6rem;
+  border-top-right-radius: 2.4rem;
+  border-top-left-radius: 2.4rem;
+  -webkit-box-sizing: border-box;
+  padding: 3.2rem 2rem env(safe-area-inset-bottom) 2rem;
+  box-sizing: border-box;
+  z-index: 101;
+  background-color: white;
+`;
+
+export const FullTypeInner = styled(motion.div)`
+  overflow: hidden;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  z-index: 3;
+  background-color: white;
+`;
+
+export const Content = styled.div`
+  overflow-x: hidden;
+  overflow-y: auto;
+  height: 100%;
+  max-height: calc(500px - 70px);
+  padding-bottom: calc(30px + env(safe-area-inset-bottom));
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+`;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,42 @@
 import React from 'react';
 
+import Modal from '@commons/Modal';
 import NaverMap from '@components/common/NaverMap';
 import HomeBottomSheet from '@components/home/BottomSheet';
 import BottomSheetHeader from '@components/home/BottomSheetHeader';
 import { AppScreen } from '@stackflow/plugin-basic-ui';
-
 import '../styles/bottomSheet.css';
+import { modalState } from '@store/modal';
+import { useAtom } from 'jotai';
 
 const Home = () => {
+  const [modal, setModal] = useAtom(modalState);
+
   return (
-    <AppScreen>
-      <NaverMap />
-      <HomeBottomSheet>
-        <div className={'container-inner'}>
-          <BottomSheetHeader />
-        </div>
-      </HomeBottomSheet>
-    </AppScreen>
+    <>
+      <Modal type={'HOME_PLACE_FILTER'} title="지역">
+        <Modal.List>asdassd</Modal.List>
+      </Modal>
+      <AppScreen>
+        <NaverMap />
+
+        <HomeBottomSheet>
+          <div className={'container-inner'}>
+            <BottomSheetHeader />
+            <button
+              onClick={() =>
+                setModal({
+                  ...modal,
+                  HOME_PLACE_FILTER: !modal.HOME_PLACE_FILTER,
+                })
+              }
+            >
+              open
+            </button>
+          </div>
+        </HomeBottomSheet>
+      </AppScreen>
+    </>
   );
 };
 
