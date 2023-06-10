20d33fb42ea29ba22137a9fb1adacbbcb9d3c3cb
Jason-Jeong-Dev <jasonjeong.dev@gmail.com>
2023-06-10 14:52:59 +0900

    fix: searchLogAtom

--- src/store/searchLogAtom.ts
+++ src/store/searchLogAtom.ts
@@ -10,6 +10,9 @@ export const searchLogAtom = atomWithStorage<SearchLog[]>('search-log', []);
 export const addSearchLogAtom = atom(null, (get, set, arg: SearchLog) => {
   const searchLog = get(searchLogAtom);
   const filterSearchLog = searchLog.filter((log) => log.id !== arg.id);
+  if (filterSearchLog.length >= 10) {
+    filterSearchLog.pop();
+  }
   const newSearchLog = [arg, ...filterSearchLog];
   set(searchLogAtom, newSearchLog);
 });
