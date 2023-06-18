25c4070a0f57290c35ade876d1e388d105949540
Hyeok_E <61281239+HyeokE@users.noreply.github.com>
2023-06-18 16:29:03 +0900

    modal atom 분리 (#24)
    Co-authored-by: Jason-Jeong-Dev <jasonjeong.dev@gmail.com>

--- src/components/common/input/SearchInput.tsx
+++ src/components/common/input/SearchInput.tsx
@@ -11,6 +11,8 @@ import {
 import classNames from 'classnames';
 import { AnimatePresence, motion } from 'framer-motion';
 
+import useComposedRef from '@hooks/useConposedRef';
+
 const easeAnimate = {
   initial: {
     opacity: 0,
@@ -27,72 +29,73 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   onSearch?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
 }
 
-const SearchInput = ({ onFocus, onBlur, onSearch, ...rest }: InputProps) => {
-  const [onFocusState, setOnFocusState] = React.useState(false);
-  const inputRef = React.useRef<HTMLInputElement>(null);
+const SearchInput = forwardRef<HTMLInputElement, InputProps>(
+  ({ onFocus, onBlur, onSearch, ...rest }, ref) => {
+    const [onFocusState, setOnFocusState] = React.useState(false);
+    const inputRef = React.useRef<HTMLInputElement>(null);
+    const composedRef = useComposedRef(inputRef, ref);
+    const handleCancel = () => {
+      if (inputRef.current === null) {
+        return;
+      }
+      inputRef.current.blur();
+      inputRef.current.value = '';
+    };
+    const onHandleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
+      setOnFocusState(true);
+      onFocus && onFocus(e);
+    };
+    const onHandleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+      setOnFocusState(false);
+      onBlur && onBlur(e);
+    };
 
-  const handleCancel = () => {
-    if (inputRef.current === null) {
-      return;
-    }
-    inputRef.current.blur();
-    inputRef.current.value = '';
-  };
-  const onHandleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
-    setOnFocusState(true);
-    onFocus && onFocus(e);
-  };
-  const onHandleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
-    setOnFocusState(false);
-    onBlur && onBlur(e);
-  };
+    const handleOnPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === 'Enter') {
+        onSearch && onSearch(e);
+      }
+    };
 
-  const handleOnPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    console.log(e.key);
-    if (e.key === 'Enter') {
-      onSearch && onSearch(e);
-    }
-  };
-
-  return (
-    <SearchInputContainer>
-      <SearchInputWrapper>
+    return (
+      <SearchInputContainer>
+        <SearchInputWrapper>
+          <AnimatePresence>
+            <Icon src={searchIcon} alt={'search-icon'} active={!onFocusState} />
+            {/*  @ts-ignore */}
+            <StyledInput
+              {...rest}
+              onFocus={onHandleFocus}
+              onBlur={onHandleBlur}
+              onKeyPress={handleOnPress}
+              active={onFocusState}
+              ref={composedRef}
+              placeholder={'음식이나 식당명을 검색하세요'}
+              className={classNames('text-l-medium')}
+            />
+          </AnimatePresence>
+        </SearchInputWrapper>
         <AnimatePresence>
-          <Icon src={searchIcon} alt={'search-icon'} active={!onFocusState} />
-          {/*  @ts-ignore */}
-          <StyledInput
-            {...rest}
-            onFocus={onHandleFocus}
-            onBlur={onHandleBlur}
-            onKeyPress={handleOnPress}
-            active={onFocusState}
-            ref={inputRef}
-            placeholder={'음식이나 식당명을 검색하세요'}
-            className={classNames('text-l-medium')}
-          />
+          {onFocusState && (
+            <motion.span
+              key={'cancel-button'}
+              className={classNames('text-m-medium', 'gray600')}
+              variants={easeAnimate}
+              initial={'initial'}
+              animate={'animate'}
+              exit={'exit'}
+              onClick={handleCancel}
+            >
+              취소
+            </motion.span>
+          )}
         </AnimatePresence>
-      </SearchInputWrapper>
-      <AnimatePresence>
-        {onFocusState && (
-          <motion.span
-            key={'cancel-button'}
-            className={classNames('text-m-medium', 'gray600')}
-            variants={easeAnimate}
-            initial={'initial'}
-            animate={'animate'}
-            exit={'exit'}
-            onClick={handleCancel}
-          >
-            취소
-          </motion.span>
-        )}
-      </AnimatePresence>
-    </SearchInputContainer>
-  );
-};
+      </SearchInputContainer>
+    );
+  },
+);
 
-const SearchInputMock = forwardRef<HTMLDivElement, InputProps>(
-  ({ ...rest }, ref) => {
+export const SearchInputMock = forwardRef<HTMLDivElement, InputProps>(
+  ({ children, ...rest }, ref) => {
     return (
       <SearchInputContainer ref={ref}>
         <SearchInputWrapper className={'input-wrapper'}>
@@ -100,7 +103,7 @@ const SearchInputMock = forwardRef<HTMLDivElement, InputProps>(
             <Icon src={searchIcon} alt={'search-icon'} active={true} />
             {/*  @ts-ignore */}
             <MockInput {...rest} className={classNames('text-l-medium')}>
-              음식이나 식당명을 검색하세요
+              {children}
             </MockInput>
           </AnimatePresence>
         </SearchInputWrapper>
@@ -108,7 +111,5 @@ const SearchInputMock = forwardRef<HTMLDivElement, InputProps>(
     );
   },
 );
-
-SearchInput.Mock = SearchInputMock;
 
 export default SearchInput;
--- src/store/modalAtom.ts
+++ src/store/modalAtom.ts
@@ -2,17 +2,41 @@ import { atom } from 'jotai';
 
 const ModalAtom = 'Modal';
 export const MODAL_KEY = {
-  HOME_PLACE_FILTER: 'HOME_PLACE_FILTER',
-  POSITION: 'POSITION',
-  CITY: 'CITY',
-  COUNTRY: 'COUNTRY',
+  IS_OPEN: 'IS_OPEN',
 } as const;
 
 export const ModalState = {
-  [MODAL_KEY.POSITION]: false,
-  [MODAL_KEY.HOME_PLACE_FILTER]: false,
-  [MODAL_KEY.CITY]: false,
-  [MODAL_KEY.COUNTRY]: false,
+  [MODAL_KEY.IS_OPEN]: false,
 };
 
 export const modalState = atom<typeof ModalState>(ModalState);
+
+export const openModal = atom(
+  null,
+  (get, set, modalKey: keyof typeof MODAL_KEY) => {
+    set(modalState, {
+      ...get(modalState),
+      [modalKey]: true,
+    });
+  },
+);
+
+export const closeModal = atom(
+  null,
+  (get, set, modalKey: keyof typeof MODAL_KEY) => {
+    set(modalState, {
+      ...get(modalState),
+      [modalKey]: false,
+    });
+  },
+);
+
+export const toggleModal = atom(
+  null,
+  (get, set, modalKey: keyof typeof MODAL_KEY) => {
+    set(modalState, {
+      ...get(modalState),
+      [modalKey]: !get(modalState)[modalKey],
+    });
+  },
+);
