299710974460d71e92befbf76fa186cd7cc1f139
jinhye <jinh2376@gmail.com>
2023-07-05 10:26:10 +0900

    chore: add Preparing

--- src/components/placeDetail/Review/index.tsx
+++ src/components/placeDetail/Review/index.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import Preparing from '@assets/mock/Preparing';
+import styled from '@emotion/styled';
+
 const Review = () => {
-  return <div>후기</div>;
+  return (
+    <ReviewWrapper>
+      <Preparing />
+    </ReviewWrapper>
+  );
 };
 
 export default Review;
+
+const ReviewWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 25vh;
+`;
