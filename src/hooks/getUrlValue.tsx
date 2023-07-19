3de0d0832fcae4c89bd92184d0ca15eb49adca59
jinhye <jinh2376@gmail.com>
2023-07-19 11:21:53 +0900

    chore: move file

--- src/hooks/getUrlValue.tsx
+++
@@ -1,9 +0,0 @@
-const getUrlValue = () => {
-  const path = window.location.pathname;
-  const split = path.split('/');
-  const value = split[2];
-
-  return Number(value);
-};
-
-export default getUrlValue;
