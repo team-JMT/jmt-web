45fd7ff5b7d4a0c6daf35651610f2a0b32158acf
jinhye <jinh2376@gmail.com>
2023-07-23 14:04:30 +0900

    chore: place info style fix

--- src/components/placeDetail/Information/index.tsx
+++ src/components/placeDetail/Information/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import useGetRestaurantDetailData from '@apis/hooks/restaurant/useGetRestaurantDetailData';
+import CopyIcon from '@assets/icons/CopyIcon';
 import NaverMapProp from '@components/common/NaverMapProp';
 import styled from '@emotion/styled';
 import { colors } from '@styles/theme/color';
@@ -30,11 +31,9 @@ const Information = () => {
         <p className={classNames('text-l-bold', 'gray900')}>
           {DetailData?.roadAddress}
         </p>
-        <AddressCopy
-          className={'text-s-medium'}
-          onClick={() => copyToClipboard(DetailData!.roadAddress)}
-        >
-          주소복사
+        <AddressCopy onClick={() => copyToClipboard(DetailData!.roadAddress)}>
+          <CopyIcon />
+          <p className={'text-s-medium'}>주소복사</p>
         </AddressCopy>
       </AddressConatiner>
       <Telephone className={'text-m-medium'}>{DetailData?.phone}</Telephone>
@@ -51,13 +50,19 @@ const AddressConatiner = styled.div`
   align-items: center;
   gap: 8px;
   margin-top: 16px;
+  p {
+    margin-top: 1px;
+  }
 `;
 const AddressCopy = styled.div`
-  text-decoration: underline;
+  display: flex;
+  align-items: center;
   color: ${colors.gray400};
+  border-bottom: 1px solid ${colors.gray400};
+  line-height: 100%;
 `;
 const Telephone = styled.div`
-  margin: 8px 0 20px 0;
+  margin: 4px 0 20px 0;
   color: ${colors.gray600};
 `;
 export default Information;
