a3d70067f7765f10b41d9602f00538e7c1945da6
HyeokE <jhjeong00@gmail.com>
2023-08-28 21:34:02 +0900

    fix: workflow

---
+++ src/pages/LocationMapPreview.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NaverMap } from 'react-naver-maps';
+
+const LocationMapPreview = () => {
+  return (
+    <div>
+      <NaverMap />
+    </div>
+  );
+};
+
+export default LocationMapPreview;
