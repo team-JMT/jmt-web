bcd0c66a0443a2649bb4b97676afe4bf6fe22338
jinhye <jinh2376@gmail.com>
2023-09-05 13:41:13 +0900

    chore: svg icon change

---
+++ src/assets/icons/ReportIcon.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+
+const ReportIcon = () => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="icon" clipPath="url(#clip0_4141_11415)">
+        <path
+          id="Vector"
+          d="M10 2.9165V2.2915"
+          stroke="#9AA9B2"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          id="Vector_2"
+          d="M15.625 4.7915L16.25 4.1665"
+          stroke="#9AA9B2"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          id="Vector_3"
+          d="M4.375 4.7915L3.75 4.1665"
+          stroke="#9AA9B2"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          id="Vector_4"
+          d="M3.75 14.7917V11.6667C3.74998 10.8419 3.9132 10.0253 4.23027 9.26386C4.54734 8.50245 5.01198 7.81133 5.5974 7.23033C6.18282 6.64934 6.87745 6.18997 7.64125 5.87869C8.40504 5.56742 9.22289 5.4104 10.0477 5.41669C13.4937 5.44247 16.25 8.29872 16.25 11.7448V14.7917"
+          stroke="#9AA9B2"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          id="Vector_5"
+          d="M10.625 7.9165C12.3984 8.21416 13.75 9.80869 13.75 11.6665"
+          stroke="#9AA9B2"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          id="Vector_6"
+          d="M16.875 14.7915H3.125C2.77982 14.7915 2.5 15.0713 2.5 15.4165V17.2915C2.5 17.6367 2.77982 17.9165 3.125 17.9165H16.875C17.2202 17.9165 17.5 17.6367 17.5 17.2915V15.4165C17.5 15.0713 17.2202 14.7915 16.875 14.7915Z"
+          stroke="#9AA9B2"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_4141_11415">
+          <rect width="20" height="20" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default ReportIcon;
