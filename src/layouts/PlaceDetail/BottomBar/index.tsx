06886cf22a50c8190acf1716d836e256795bb397
jinhye <jinh2376@gmail.com>
2023-10-12 14:07:53 +0900

    chore: initial link change

--- src/layouts/PlaceDetail/BottomBar/index.tsx
+++ src/layouts/PlaceDetail/BottomBar/index.tsx
@@ -18,7 +18,7 @@ const BottomBar = ({ name, x, y }: Props) => {
   const userLocation = nativeInfo.getData().userPosition;
 
   const LinkSearchRoad = () => {
-    window.location.href = `nmap://route/public?slat=${userLocation.y}&slng=${userLocation.x}&sname=내위치&dlat=${y}&dlng=${x}&dname=${name}`;
+    window.location.href = `http://m.map.naver.com/route.nhn?menu=route&sname=내위치&sx=${userLocation.x}&sy=${userLocation.y}&ename=${name}&ex=${x}&ey=${y}&pathType=0&showMap=true`;
     //네이버 앱으로 길찾기 실행
     //`nmap://route/public?slat=${userLocation.y}&slng=${userLocation.x}&sname=내위치&dlat=${y}&dlng=${x}&dname=${name}`
     //모바일 웹 , 길찾기 화면으로 이동은 되는데 위치 설정이 아무것도 안돼요..
